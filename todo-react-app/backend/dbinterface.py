08ea2b1a366d9627067c3d6cef991b0bd2f08de8
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-07-26 09:31:20 -0400

    Initial DB test

--- todo-react-app/backend/dbinterface.py
+++ todo-react-app/backend/dbinterface.py
@@ -9,7 +9,7 @@ def _task_to_document(task: Task | CreateTask):
 
 
 class MongoDBInterface:
-    def __init__(self, table_name: str = "todo_app"):
+    def __init__(self, db_name: str = "todo_app"):
         # Provide the mongodb atlas url to connect python to mongodb using pymongo
         CONNECTION_STRING = "mongodb://localhost:27017/"
 
@@ -17,7 +17,7 @@ class MongoDBInterface:
         self._client = MongoClient(CONNECTION_STRING)
 
         # Create the database for our example (we will use the same database throughout the tutorial
-        self._db = self._client[table_name]
+        self._db = self._client[db_name]
         self._task_collection = self._db["tasks"]
 
     def create_task(self, task: CreateTask) -> Task:
@@ -25,6 +25,9 @@ class MongoDBInterface:
         return Task(
             id=str(result.inserted_id), name=task.name, isCompleted=task.isCompleted
         )
+
+    def num_tasks(self):
+        return self._task_collection.count_documents({})
 
     def print_tasks(self):
         for task in self._task_collection.find():
---
+++ todo-react-app/backend/test/test_dbinterface.py
@@ -0,0 +1,26 @@
+import pytest
+
+from backend.dbinterface import MongoDBInterface
+from backend.task import CreateTask, Task
+
+DB_NAME = "test_todo_app"
+
+
+@pytest.fixture
+def get_db():
+    db = MongoDBInterface(DB_NAME)
+    assert db.num_tasks() == 0
+    yield db
+    db._task_collection.drop()
+    assert db.num_tasks() == 0
+
+
+def test_create_task(get_db):
+    db = get_db
+    assert db.num_tasks() == 0
+    task_info = CreateTask(name="Task 1", isCompleted=False)
+    new_task = db.create_task(task_info)
+    assert task_info.name == new_task.name
+    assert task_info.isCompleted == new_task.isCompleted
+    assert new_task.id is not None
+    assert new_task.id != ""
